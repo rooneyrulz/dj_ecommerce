df98b087f7ede961958f55d3d3fd63f4a7ea97c0
rooneyrulz <roony0782045336@gmail.com>
2019-09-18 07:36:12 +0530

    added requirement.txt file & setup basic urls for pages

--- src/djecommerce/settings.py
+++ src/djecommerce/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party Apps
+
+    # Own Apps
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+# Landing Page View
+def landing_page_view(request):
+  return HttpResponse('<h1>Landing Page</h1>')
+
+
+# About Page View
+def about_page_view(request):
+  return HttpResponse('<h1>About Page</h1>')
