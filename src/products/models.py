507cfafb35daa7d3edb1fc52d0f98c5c96feac37
rooneyrulz <roony0782045336@gmail.com>
2019-09-23 13:46:45 +0530

    customized admin & changed product model

--- src/products/models.py
+++ src/products/models.py
@@ -1,24 +1,39 @@
-from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
+
 class Product(models.Model):
-  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  name = models.CharField(max_length=100)
-  price = models.DecimalField(max_digits=1000, decimal_places=2)
-  description = models.TextField(default="It's awesome product..")
-  published_at = models.DateTimeField(auto_now_add=True)
-  image = models.ImageField(upload_to='product_images')
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(
+      max_digits=1000,
+      decimal_places=2
+    )
+    description = models.TextField(
+      default="It's awesome product..",
+      blank=True,
+      null=True
+    )
+    stock = models.IntegerField(default=1)
+    published_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='product_images')
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  def get_absolute_url(self, *args, **kwargs):
-    return reverse('products:product_details_view', kwargs={'pk':self.id})
+    def get_absolute_url(self, *args, **kwargs):
+        return reverse(
+          'products:product_details_view',
+          kwargs={'pk': self.id}
+        )
 
-  def get_edit_url(self, *args, **kwargs):
-    return reverse('products:product_update_view', kwargs={'pk':self.id})
+    def get_edit_url(self, *args, **kwargs):
+        return reverse(
+          'products:product_update_view',
+          kwargs={'pk': self.id}
+        )
 
-  
-  def get_add_to_cart_url(self, *args, **kwargs):
-    return reverse('products:product_add_to_cart_view', kwargs={'pk':self.id})
+    def get_add_to_cart_url(self, *args, **kwargs):
+        return reverse(
+          'products:product_add_to_cart_view',
+          kwargs={'pk': self.id}
+        )
--- src/products/views.py
+++ src/products/views.py
@@ -1,30 +1,45 @@
 from django.shortcuts import render
 
+from .models import Product
+from .forms import ProductForm
+
+
 # List out all products
 def product_list_view(request):
-  return render(request, 'products/product_list.html', {})
+	products = Product.objects.all()
+	context = {
+		'products': products
+	}
+	return render(request, 'products/product_list.html', context)
 
 
 # Products details view
 def product_details_view(request, pk):
-  return render(request, 'products/product_details.html', {})
+	return render(request, 'products/product_details.html', {})
 
 
 # Product create view
 def product_create_view(request):
-  return render(request, 'products/product_create.html', {})
+	form = ProductForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		form.save()
+		form = ProductForm()
+	context = {
+		'form': form
+	}
+	return render(request, 'products/product_create.html', context)
 
 
 # Product update view
 def product_update_view(request, pk):
-  return render(request, 'products/product_update.html', {})
+	return render(request, 'products/product_update.html', {})
 
 
 # Product add to cart view
 def product_add_to_cart_view(request, pk):
-  return render(request, 'products/product_add_to_cart.html', {})
+	return render(request, 'products/product_add_to_cart.html', {})
 
 
 # Product delete view
 def product_delete_view(request, pk):
-  return render(request, 'products/product_delete.html', {})
+	return render(request, 'products/product_delete.html', {})
